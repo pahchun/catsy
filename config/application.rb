86a9fe9d080379d363830a0d3605d2191c6e3074
Wendy H. Chun <wendyhchun@wendy-chuns-macbook-pro.local>
2016-04-27 08:20:13 -0700

    first commit

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module Btoy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
